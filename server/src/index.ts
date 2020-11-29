aa1b6377b6d66c037b366f8457abec4c7a6c4d7d
Maria Inês Serra <14142563+mariaines00@users.noreply.github.com>
2020-11-29 13:37:47 +0000

    Get server up and running

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,21 @@
-console.log("Bom dia alegria");
+import express, { Request, Response, NextFunction} from "express";
+
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req: Request, res: Response) => {
+    res.send(`
+        <div>
+            <h1> Bom dia alegria </h1>
+        </div>
+    `);
+})
+
+//error handler middleware
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    res.status(500).json({ message: err.message });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`)
+});
